177c093a5c8fa4cc4e55e7c7a0065dd415e7b7cd
DuilioAzevedo <duilio_uei@hotmail.com>
2025-01-28 00:07:22 -0300

    change random color

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const botao = document.getElementById("botao");
 
+function gerarCorAleatoria() {
+  const r = Math.floor(Math.random() * 256); // Valor entre 0 e 255
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`; // Formato RGB
+}
+
 botao.addEventListener("click", function () {
   const conteiner = document.getElementById("conteiner");
-  conteiner.style.backgroundColor = "lightblue";
-  
+  const corAleatoria = gerarCorAleatoria();
+  conteiner.style.backgroundColor = corAleatoria;
 });
